8cd7492171c22a1a88148ebad64941380f02889d
parris3142 <parris3142@hotmail.com>
2015-06-15 21:47:06 +0100

    Assignment Commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,56 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(this.mat = matrix()) {
+        this.inv <- NULL
+        set <- function(y) {
+                this.mat <<- y
+                this.inv <<- NULL
+        }
+        get <- function() this.mat
+        setinv <- function(inverse) this.inv <<- inverse
+        getinv <- function() this.inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMat, ...) {
+        inv <- cacheMat$getinv()
+        if(!is.null(inv)) {
+                message("   ***returning cached inversion")
+                return(inv)
+        }
+        mat <- cacheMat$get()
+	  message("   *** Solve, as no cached inversion")
+        inv <- solve(mat, ...)
+        cacheMat$setinv(inv)
+        inv
+}
+
+## Simple test function.
+
+test <- function()
+{
+   message("Create test 3x3 matrix")
+   testM <- matrix(c(1,2,3,0,1,4,5,6,0),ncol=3,byrow=TRUE)
+
+   message("Create instance of CacheMatrix")
+   cm <- makeCacheMatrix(testM)
+
+   message("Solve, first time so will not get from cache")
+   sol <- cacheSolve(cm)
+
+   message("Solve, second time so will get from cache")
+   sol <- cacheSolve(cm)
+
+   message("Put new matrix into CacheMat so will have to resolve")
+   cm <- makeCacheMatrix(sol)
+   orig <- cacheSolve(cm)
+
+   message("we passed in the inverse to be inverted again so we should be back at the original")
+   return(round(orig) == round(testM))
 }
