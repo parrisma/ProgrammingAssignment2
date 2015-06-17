23f3fb94e7de1e42b9e70b624697336888593486
parris3142 <parris3142@hotmail.com>
2015-06-17 20:07:49 +0100

    Added additonal comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,50 @@
-## Put comments here that give an overall description of what your
+## The package provides two functions that can be used together
+## ensure that the computational expense of calculating the inverse
+## of a matrix is only done the first time the inverse value is used.
+## Susiquent uses of the inversion will recall the value from a cache
+## rather then re run the inversion computation.
 ## functions do
 
-## Write a short comment describing this function
+## The function allows the inversion of a matrix to be stored
+## after the value is calculated. If the input matrix is updated
+## this function will ensure the the now redundent cached inversion
+## is deleted such that a new inversion is executed for the new
+## input value.
 
 makeCacheMatrix <- function(this.mat = matrix()) {
+        ## This holds the inverted matrix once calculated
         this.inv <- NULL
+
+	  ## This allows the input matrix to be set and ensures
+        ## that any stored results from previous input values
+        ## are delted.
         set <- function(y) {
                 this.mat <<- y
                 this.inv <<- NULL
         }
+        ## This allows the value of the input to be obtained
         get <- function() this.mat
+        ## This stored the inverted matrix once calculated
         setinv <- function(inverse) this.inv <<- inverse
+	  ## This allows the inverted matrix to be obtained.
         getinv <- function() this.inv
+	  ## This exposes the functions as named methods of this object.
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function takes as input a CacheMatrix and for the first request for
+## its inverted form will calculate the result and store that result in the
+## input cacheMatrix. For subsiquent calls to get the inverted form the function
+## will recover the previous value from cacheMatrix rather than calculate the
+## inverted form again.
 
 cacheSolve <- function(cacheMat, ...) {
+	  ## Get the inverted form of the input matrix from cache matrix
+        ## if the return value is null, this means that there is no cached
+        ## result and we have to calculate it and store it back to cacheMatrix.
         inv <- cacheMat$getinv()
         if(!is.null(inv)) {
                 message("   ***returning cached inversion")
@@ -33,7 +57,8 @@ cacheSolve <- function(cacheMat, ...) {
         inv
 }
 
-## Simple test function.
+## This is a test function that exercies the functions above to prove they
+## behave as required.
 
 test <- function()
 {
